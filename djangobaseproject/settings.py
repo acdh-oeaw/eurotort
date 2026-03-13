14baa99a1aa51e19964c5cdb66fea23e670d4049
csae8092 <p.andorfer@gmail.com>
2026-03-13 14:53:48 +0100

    chore: removed default value for DJANGO_SECRET_KEY env variable

--- djangobaseproject/settings.py
+++ djangobaseproject/settings.py
@@ -14,7 +14,7 @@ REDMINE_ID = os.environ.get("REDMINE_ID", 21961)
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "rlYWFQbF")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get("DEBUG"):
