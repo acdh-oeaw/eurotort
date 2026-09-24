fbf0d6e869c0abccd57b0da90c3fc3ab3d55ba88
csae8092 <p.andorfer@gmail.com>
2026-09-24 11:47:40 +0200

    chore: always run migrations on server start [skip ci]

--- start-server.sh
+++ start-server.sh
@@ -3,15 +3,5 @@
 echo "Hello from Project Eurotort - Database on European Tort Law"
 
 uv run manage.py collectstatic --no-input
-if [ -n "$MIGRATE" ] ; then
-    (echo "making migrations and running them"
-    
-    uv run manage.py makemigrations --no-input
-    
-    uv run manage.py migrate --no-input)
-fi
-if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (echo "creating superuser ${DJANGO_SUPERUSER_USERNAME}" && 
-    uv run manage.py createsuperuser --no-input --noinput --email 'blank@email.com')
-fi
+uv run manage.py migrate --no-input
 uv run gunicorn djangobaseproject.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3 & nginx -g "daemon off;"
